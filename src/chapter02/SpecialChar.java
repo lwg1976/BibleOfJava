6acf255db993914f444088d3bc0aec8757162e9c
lwg1976 <lwg1976@lycos.co.kr>
2014-10-22 21:39:48 +0900

    예제 2-3 특수문자 출력

---
+++ src/chapter02/SpecialChar.java
@@ -0,0 +1,17 @@
+package chapter02;
+
+public class SpecialChar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		char single = '\''; 			// single = ''';와 같이 할 수 없다.
+		String dblQuote = "\"Hello\"";	// 큰따옴표를 출력하려면 이렇게 한다.
+		String root = "c:\\";
+		
+		System.out.println(single);
+		System.out.println(dblQuote);
+		System.out.println(root);		
+	}
+}
