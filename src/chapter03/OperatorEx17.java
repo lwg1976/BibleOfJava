2a7ec1755ddc636ca38764faf10ea03cae00d937
lwg1976 <lwg1976@lycos.co.kr>
2014-10-22 23:36:04 +0900

    예제 3-17 소수점 아래 자리 맞추기

---
+++ src/chapter03/OperatorEx17.java
@@ -0,0 +1,14 @@
+package chapter03;
+
+public class OperatorEx17 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		float pi = 3.141592f;
+		float shortPi = (int)(pi * 1000) / 1000f;
+		
+		System.out.println(shortPi);
+	}
+}
