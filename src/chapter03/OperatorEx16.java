9b8547dc1bf18d496bd9f886f3e2dedb982ba029
lwg1976 <lwg1976@lycos.co.kr>
2014-10-22 23:34:11 +0900

    예제 3-16 사칙 연산자9 대문자로 변환

---
+++ src/chapter03/OperatorEx16.java
@@ -0,0 +1,13 @@
+package chapter03;
+
+public class OperatorEx16 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		char lowerCase = 'a';
+		char upperCase = (char)(lowerCase - 32);
+		System.out.println(upperCase);
+	}
+}
