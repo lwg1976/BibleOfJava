4afe521f16f91a7e9e7f8284642a922da275e6ad
lwg1976 <lwg1976@lycos.co.kr>
2014-10-22 23:01:18 +0900

    예제 3-5 비트전환 연산자1

---
+++ src/chapter03/OperatorEx5.java
@@ -0,0 +1,14 @@
+package chapter03;
+
+public class OperatorEx5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		byte b = 10;
+		System.out.println("b=" + b);
+		System.out.println("~b=" + ~b);
+		System.out.println("~b+1=" + (~b+1));
+	}
+}
