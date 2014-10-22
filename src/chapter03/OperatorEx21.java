af5c7b8970c1d795836daa4e720c191c2892113a
lwg1976 <lwg1976@lycos.co.kr>
2014-10-22 23:47:45 +0900

    예제 3-21 나눗셈과 부호

---
+++ src/chapter03/OperatorEx21.java
@@ -0,0 +1,10 @@
+package chapter03;
+
+public class OperatorEx21 {
+
+	public static void main(String[] args) {
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+	}
+}
