bc9ee10b5e3eeacc63982c90aad4ccd53ba31103
lwg1976 <lwg1976@lycos.co.kr>
2014-10-22 23:15:50 +0900

    예제 3-11 사칙 연산자4

---
+++ src/chapter03/OperatorEx11.java
@@ -0,0 +1,14 @@
+package chapter03;
+
+public class OperatorEx11 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		long a = 1000000 * 1000000;
+		long b = 1000000 * 1000000L;	// long형 리터럴
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
