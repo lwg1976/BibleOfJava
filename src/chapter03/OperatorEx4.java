9631eefd3331edb0d8712963be484f2052cb04dd
lwg1976 <lwg1976@lycos.co.kr>
2014-10-22 22:59:20 +0900

    예제 3-4 부호 연산자

---
+++ src/chapter03/OperatorEx4.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class OperatorEx4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int i = -10;
+		i = +i;
+		System.out.println(i);
+		i = -10;
+		i = -i;
+		System.out.println(i);
+	}
+}
