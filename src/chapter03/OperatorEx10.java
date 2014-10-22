8505e6a631e09212e8d9050de56108437063ddac
lwg1976 <lwg1976@lycos.co.kr>
2014-10-22 23:14:11 +0900

    예제 3-10 사칙 연산자3

---
+++ src/chapter03/OperatorEx10.java
@@ -0,0 +1,15 @@
+package chapter03;
+
+public class OperatorEx10 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 1000000;	// 1,000,000	1백만
+		int b = 2000000;	// 2,000,000	2백만
+		long c = a * b;		// 2,000,000,000,000
+		System.out.println(c);
+	}
+
+}
