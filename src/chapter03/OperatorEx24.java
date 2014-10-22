7278d8e14e15ec9cf7004fcf90324dee960cc3df
lwg1976 <lwg1976@lycos.co.kr>
2014-10-23 00:05:34 +0900

    예제 3-24 float와 double의 소수의 비교

--- src/chapter03/OperatorEx24.java
+++ src/chapter03/OperatorEx24.java
@@ -3,22 +3,17 @@ package chapter03;
 public class OperatorEx24 {
 
 	public static void main(String[] args) {
-		if(10 == 10.0f) {
-			System.out.println("10과 10.f는 같다.");
-		}
+		float f = 0.1f;
+		double d = 0.1;
+		double d2 = (double)f;
 		
-		if('0' != 0) {
-			System.out.println("'0'과 0은 같지 않다.");
-		}
-		
-		if('A' == 65) {
-			System.out.println("'A'와 65는 같다.");
-		}
-		
-		int num = 5;
-		
-		if(num > 0 && num < 9) {
-			System.out.println("5는 0보다 크고, 9보다는 작다.");
-		}
+		System.out.println("10.0==10.0f ? " + (10.0==10.0f));
+		System.out.println("0.1==0.1f ? " + (0.1==0.1f));
+		System.out.println("f=" + f);
+		System.out.println("d=" + d);
+		System.out.println("d2=" + d2);
+		System.out.println("d==f ? " + (d==f));
+		System.out.println("d==d2 ? " + (d==d2));
+		System.out.println("d2==f ? " + (d2==f));
 	}
 }
