561f6a42a42ba5748392823abf1172964cb3d2a7
lwg1976 <lwg1976@lycos.co.kr>
2014-10-24 00:37:54 +0900

    예제 5-14 매개변수를 이용하여 모스부호 출력

---
+++ src/chapter05/ArrayEx14.java
@@ -0,0 +1,28 @@
+package chapter05;
+
+public class ArrayEx14 {
+
+	public static void main(String[] args) {
+		if (args. length != 1) {
+			System.out.println("usage: java MorseConverter Word");
+			System.exit(0);
+		}
+		
+		System.out.println("source:" + args[0]);
+		String source = args[0].toUpperCase();
+		
+		String[] morse = {".-", "-...", "-.-.", "-..", "."
+						, "..-.", "--.", "....", "..", ".---"
+						, "-.-", ".-..", "--", "-.", "---"
+						, ".--.", "--.-", ".-.", "...", "-"
+						, "..-", "...-", "-..-", "-.--"
+						, "--.." };
+		String result = "";
+		
+		for(int i=0; i<source.length(); i++) {
+			result += morse[source.charAt(i)-'A'];
+		}
+		
+		System.out.println("morse:" + result);
+	}
+}
