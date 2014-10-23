972b8dd43a78bbfc362064059850d2c52d21c0b4
lwg1976 <lwg1976@lycos.co.kr>
2014-10-24 00:32:56 +0900

    예제 5-13 매개변수를 이용한 배열

---
+++ src/chapter05/ArrayEx13.java
@@ -0,0 +1,11 @@
+package chapter05;
+
+public class ArrayEx13 {
+
+	public static void main(String[] args) {
+		System.out.println("매개변수의 개수:" + args.length);
+		for(int i=0; i<args.length;i++) {
+			System.out.println("args[" + i + "] = \"" + args[i] + "\"");
+		}
+	}
+}
