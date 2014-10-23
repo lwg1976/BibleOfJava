1dc11f7b0d73eca3af5e5c4cc452da72d1349ab9
lwg1976 <lwg1976@lycos.co.kr>
2014-10-23 22:32:10 +0900

    예제 4-14 예제 4-13을 수정한 것

---
+++ src/chapter04/FlowEx14.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+public class FlowEx14 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int sum = 0;
+		int i;
+		
+		for(i=1; i<=10; i++) {
+			sum += i;
+		}
+		System.out.println(i-1 + " 까지의 합; " + sum);
+
+	}
+
+}
