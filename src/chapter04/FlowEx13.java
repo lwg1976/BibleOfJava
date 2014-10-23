48dfdf27f6f91705c4beaa0a9738eb8dcfa05e42
lwg1976 <lwg1976@lycos.co.kr>
2014-10-23 22:30:12 +0900

    예제 4-13 변수가 범위를 넘는 경우

---
+++ src/chapter04/FlowEx13.java
@@ -0,0 +1,18 @@
+package chapter04;
+
+public class FlowEx13 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=1; i<=10; i++) {
+			sum += i;
+		}
+		System.out.println(i-1 + " 까지의 합: " + sum);
+
+	}
+
+}
