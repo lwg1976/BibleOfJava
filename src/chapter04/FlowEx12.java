6c0af6c0bd9d6a31391cab4f9acad1cc4648042f
lwg1976 <lwg1976@lycos.co.kr>
2014-10-23 22:27:27 +0900

    예제 4-12 for 문

---
+++ src/chapter04/FlowEx12.java
@@ -0,0 +1,18 @@
+package chapter04;
+
+public class FlowEx12 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=1; i<=10; i++) {
+			sum += i;
+			System.out.println(i + " 까지의 합: " + sum);
+		}
+
+	}
+
+}
